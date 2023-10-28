566c07ce73569e33a12e8df101ab758261afa47d
Ethan Duong <eduong100@gmail.com>
2023-10-28 11:29:02 -0700

    Clean up code

--- hw3_prime_numbers/primes.c
+++ hw3_prime_numbers/primes.c
@@ -1,63 +1,11 @@
-/* SUBMIT ONLY THIS FILE */
-/* NAME: ....... */
-/* UCI ID: .......*/
+/* NAME: Ethan Duong, Grant Kinsley */
+/* UCI ID: 16765837, */
 
 #include <pthread.h>
 #include "util_common.h"
-#include "primes.h"//implementing
-
-
-
-//>>>>>>>> DELETE CODE FROM THIS POINT
-#ifdef DEBUG
-/**
- * This is just a demo code to show the use of the concurrent
- * array type and its functions (carr_d_xxxx). For more info,
- * read the comments in carr_d.h
- **/
+#include "primes.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-void demo_code(carr_d_t *my_array, unsigned int max){
-    unsigned int x=0;
-    srand((unsigned) time(NULL));
-    printf("Empty array:\n");
-    carr_d_print(my_array);
-
-    unsigned int i;
-    for(i=0; i<10; i++){
-        x = rand() % max;
-        printf("Pushing %d.\n", x);
-        carr_d_push(my_array, x);
-        carr_d_print(my_array);
-    }
-}
-#endif
-//<<<<<<<< DELETE UP TO THIS POINT
-
-// Return 1 if num is prime
-// Else return 0
-int isPrime(unsigned int num) {
-    if (num <= 1) {
-        return 0;
-    }
-    for (unsigned int i = 2; i <= num/2; i++) {
-        if (num % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void primes_sequential(carr_d_t *primes_list, unsigned int max){
-    for (unsigned int i = 1; i <= max; i++) {
-        if(isPrime(i)) {
-            carr_d_push(primes_list, i);
-        }
-    }
-    // carr_d_print(primes_list);
-    return;
-}
 
 typedef struct {
     pthread_mutex_t *pmtx;
@@ -66,6 +14,25 @@ typedef struct {
     unsigned int max;
 } Args;
 
+int isPrime(unsigned int num) {
+    if (num <= 1) 
+        return 0;
+
+    for (unsigned int i = 2; i <= num/2; i++) {
+        if (num % i == 0) 
+            return 0;
+    }
+
+    return 1;
+}
+
+void primes_sequential(carr_d_t *primes_list, unsigned int max){
+    for (unsigned int i = 1; i <= max; i++) {
+        if(isPrime(i)) 
+            carr_d_push(primes_list, i);
+    }
+}
+
 void* generatePrimes(void *args) {
     Args *a = (Args *) args;
     while (1) {
@@ -73,24 +40,24 @@ void* generatePrimes(void *args) {
         unsigned int curNum = *(a->counter);
         *(a->counter) = *(a->counter) + 1;
         pthread_mutex_unlock(a->pmtx);
-        if (curNum > a->max) {
+
+        if (curNum > a->max) 
             pthread_exit(NULL);
-        }
-        if(isPrime(curNum)) {
+
+        if(isPrime(curNum)) 
             carr_d_push(a->primes_list, curNum);
-        }
     }
     pthread_exit(NULL);
 }
 
-void primes_parallel(carr_d_t *primes_list, unsigned int max, \
-                     unsigned int thr){
-        
+void primes_parallel(carr_d_t *primes_list, unsigned int max, unsigned int thr) {
     unsigned int counter = 1;
     pthread_t threads[thr];
     pthread_mutex_t pmtx;
-    Args arg_thr = { &pmtx, &counter, primes_list, max };
     pthread_mutex_init(&pmtx, NULL);
+
+    Args arg_thr = { &pmtx, &counter, primes_list, max };
+    
     for (unsigned int i = 0; i < thr; i++) {
         int ret = pthread_create(&threads[i], NULL, &generatePrimes, (void*) &arg_thr);
         if(ret) {
@@ -98,8 +65,8 @@ void primes_parallel(carr_d_t *primes_list, unsigned int max, \
             exit(-1);
         }
     }
+
     for (unsigned int i = 0; i < thr; i++) {
         pthread_join(threads[i], NULL);
     }
-    // carr_d_print(primes_list);
 }
