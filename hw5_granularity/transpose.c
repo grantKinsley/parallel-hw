8b095b7038572b63eb47b4ab2f3a398a5d595350
Grant Kinsley <grantkinsley@g.ucla.edu>
2023-11-12 14:40:46 -0800

    clean up code and add report

--- hw5_granularity/transpose.c
+++ hw5_granularity/transpose.c
@@ -43,38 +43,6 @@ static void swap_entries(Mat *mat, unsigned int row, unsigned int col) {
     mat->ptr[col*size+row] = temp;
 }
 
-// static void* transpose_mat(void *args) {
-//     // printf("swap entries\n");
-//     Args *a = (Args *) args;
-//     unsigned int size = a->mat->m;
-    
-//     while(1) {
-//         // get the entry of mat to work on
-//         pthread_mutex_lock(a->pmtx);
-//         unsigned int curCounter = *(a->counter);
-//         *(a->counter) = *(a->counter) + a->grain;
-//         pthread_mutex_unlock(a->pmtx);
-
-//         if (curCounter >= (size*(size-1))/2) {
-//             pthread_exit(NULL);
-//         }
-
-//         for (unsigned int k = 0; k < a->grain; k++) {
-//             unsigned int curEntry = a->entriesToSwap->arr[curCounter];
-//             unsigned int row = curEntry / size;
-//             unsigned int col = curEntry % size;
-//             swap_entries(a->mat, row, col);
-            
-//             curCounter++;
-//             if (curCounter >= (size*(size-1))/2) {
-//                 break;
-//             }
-//         }
-//     }
-//     printf("end thread\n");
-//     pthread_exit(NULL);
-// }
-
 static void* transpose_mat(void *args) {
     Args *a = (Args *) args;
     unsigned int size = a->mat->m;
@@ -96,10 +64,7 @@ static void* transpose_mat(void *args) {
             }
             unsigned int remainder = (*(a->colCounter)+1) - size;
             *(a->colCounter) = *(a->rowCounter) + remainder;
-            // printf("counter: %d %d\n", *(a->rowCounter), *(a->colCounter));
         }
-        // printf("current: %d %d\n", curRow, curCol);
-        // printf("counter: %d %d\n", *(a->rowCounter), *(a->colCounter));
 
         pthread_mutex_unlock(a->pmtx);
 
@@ -110,7 +75,6 @@ static void* transpose_mat(void *args) {
 
         // do grain number of swaps
         for (unsigned int k = 0; k < a->grain; k++) {
-            // printf("swap %d %d\n", curRow, curCol);
             swap_entries(a->mat, curRow, curCol);
             ++curCol;
             if (curCol >= size) {
@@ -122,7 +86,6 @@ static void* transpose_mat(void *args) {
                 break;
             }
         }
-        // printf("\n");
     }
     pthread_exit(NULL);
 }
@@ -136,19 +99,7 @@ void mat_squaretransp_parallel(Mat *mat, unsigned int grain, unsigned int thr){
     unsigned int rowCounter = 0;
     unsigned int colCounter = 1;
 
-    // unsigned int counter = 0;
-    // carr_d_t entriesToSwap; // holds the entries of upper triangle of mat
-    // carr_d_init(&entriesToSwap, (size*(size-1))/2);
-
-    // for (unsigned int i = 0; i < size; i++) {
-    //     for (unsigned int j = i+1; j < size; j++) {
-    //         carr_d_push(&entriesToSwap, i*size+j);
-    //     }
-    // }
-
     Args arg_thr = { &pmtx, grain, mat, &rowCounter, &colCounter};
-
-    // Args arg_thr = { &pmtx, grain, &counter, &entriesToSwap, mat };
 
     for (unsigned int i = 0; i < thr; i++) {
         int ret = pthread_create(&threads[i], NULL, &transpose_mat, (void*) &arg_thr);
